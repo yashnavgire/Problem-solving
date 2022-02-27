7f367a3ba3d9e5a330f4e0eb2cd134a3b9a7cfca
Amit Jaydeepsingh Thakur <thakuramitjaydeep@gmail.com>
2022-02-27 16:55:00 +0530

    Create shortestPathwithUnitDistanceWeights.java

---
+++ JAVA/Graphs/shortestPathwithUnitDistanceWeights.java
@@ -0,0 +1,26 @@
+public void shortestPath(List<List<Integer>> adj, int[] dist, int src) {
+    int i = 0, top = 0, parent = 0;
+    Arrays.fill(dist, -1);
+
+    Queue<Integer> q = new LinkedList<>();
+
+    q.offer(src);
+    dist[src] = 0;
+
+    while (!q.isempty()) {
+        top = q.poll();
+        parent = dist[top];
+
+        List<Integer> list = adj.get(top);
+
+        for (int ele : list) {
+            if (dist[ele] == Integer.MAX_VALUE) {
+                dist[ele] = parent + 1;
+                q.offer(ele);
+            }
+            else {
+                dist[ele] = Math.min(parent + 1, dist[ele]);
+            }
+        }
+    }
+}
