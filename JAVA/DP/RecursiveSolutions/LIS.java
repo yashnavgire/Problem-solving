7291243e9c71b1e22ff8c66740e931e656b9e7fa
Amit Jaydeepsingh Thakur <thakuramitjaydeep@gmail.com>
2022-03-12 12:46:49 +0530

    Update LIS.java

--- JAVA/DP/RecursiveSolutions/LIS.java
+++ JAVA/DP/RecursiveSolutions/LIS.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
+        int[] dp = new int[nums.length + 1];
         Arrays.fill(dp, -1);
         
         return solve(nums, dp, 0, Integer.MIN_VALUE);
@@ -10,17 +10,15 @@ class Solution {
         if (i >= nums.length) {
             return 0;
         }
-        
-        if (dp[i] != -1) {
+         
+        if (nums[i] > prev) {
+            
+            dp[i] = (Math.max(1 + 
+                             solve(nums, dp, i + 1, nums[i]), 
+                             solve(nums, dp, i + 1, prev)));
             return dp[i];
         }
         
-        int take = 0;
-        int dontTake = solve(nums, dp, i + 1, prev);
-        if(nums[i] > prev) {
-            take = 1 + solve(nums, dp, i + 1, nums[i]); 
-        }
-        dp[i] = Math.max(take, dontTake);
-        return  dp[i];                               
+        return solve(nums, dp, i + 1, prev);
     }
 }
