f809def30046811628e9fd9ebf1541ea0d5e83f6
Amit Jaydeepsingh Thakur <thakuramitjaydeep@gmail.com>
2022-05-07 23:26:33 +0530

    Update distinctNumbersInWindow.java

--- JAVA/Sliding Window/distinctNumbersInWindow.java
+++ JAVA/Sliding Window/distinctNumbersInWindow.java
@@ -1,10 +1,11 @@
 class Solution {
 	int[] distintNumbersInWindow(int[] A, int k) {
 		
-	    int i = 0, j = 0;
+	    int i = 0, j = 0, m = 0;
 		Set<Integer> set = new HashSet<>();
 		Map<Integer, Integer> map = new HashMap<>();
 		List<Integer> list = new ArrayList<>();
+		int[] ans = new int[A.length - k + 1];
 		
 		while (j <= A.length - 1) {
 			if (!map.containsKey(A[j])) {
@@ -13,28 +14,23 @@ class Solution {
 			else {
 				map.put(A[j], map.get(A[j]) + 1);
 			}
-			// set.add(A[j]);
+			
 			if ((j - i + 1) < k) {
 				j++;
 			}
 			else if((j - i + 1) == k) {
 				list.add(map.size());
+				ans[m++] = map.size();
 				if (map.get(A[i]) == 1) {
 					map.remove(A[i]);
 				}
 				else {
 					map.put(A[i], map.get(A[i]) - 1);
 				}
-				// set.remove(A[i]);
+				
 				i++;
 				j++;
 			}
-		}
-    
-		int[] ans = new int[list.size()];
-		
-		for (i = 0; i < list.size(); i++) {
-			ans[i] = list.get(i);
 		}
 		
 		return ans;
